0e0fc40a1b9c3e5451d694f1c205b0e73e687f4f
jcshim <jcshim@andong.ac.kr>
2019-11-28 00:33:33 +0900

    초기 커밋.

---
+++ MainPage.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+// https://docs.microsoft.com/ko-kr/windows/uwp/cpp-and-winrt-apis/simple-winui-example 
+namespace winrt::BlankApp2::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        //myButton().Content(box_value(L"Clicked"));
+    }
+}
